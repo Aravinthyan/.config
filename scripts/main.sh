a11981c12e36f1438c9e867a6fe44590d83d86a8
aravinthyan <aravinthyan@outlook.com>
2021-05-10 22:01:28 +0100

    Initial dmenu main.sh script
    - The script will be able to mount and unmount partitions and provide
      approproate notifications as well.
    
    - Keybinding not assigned to script

---
+++ scripts/main.sh
@@ -0,0 +1,75 @@
+export dmenu_command='dmenu -i -l 10 -nb '#000000' -nf '#ffffff' -sb '#ff8c00' -sf '#ffffff' -bw 3'
+
+main_prompt="1) Run application\n2) Mount partition\n3) Umount partition"
+
+choice=$(echo -e $main_prompt | $dmenu_command -p "Choose option: ")
+
+if [[ $choice = '1) Run application' ]]
+then
+    dmenu_run
+elif [[ $choice = "2) Mount partition" ]]
+then
+    mount_prompt="1) Non-encrypted\n2) Encrypted"
+
+    choice=$(echo -e $mount_prompt | $dmenu_command -p "Choose Non-encrypted or Encrypted: ")
+
+    if [[ $choice = "1) Non-encrypted" ]]
+    then
+        choosen_part=$(lsblk -lp | grep "part $" | awk '{print $1, "(" $4 ")"}' | \
+            $dmenu_command -p "Choose partition: ")
+        
+        if [[ $choosen_part = "" ]]
+        then
+        	exit 0
+        fi
+        
+        choosen_part=$(echo $choosen_part | awk '{print $1}')
+
+        mount_point=$(echo -e "/mnt/\n/media/" | $dmenu_command -p "Enter mount point: ")
+        
+        if [[ $mount_point != "" ]]
+        then
+        	sudo mkdir -p $mount_point 2>/dev/null
+            sudo mount $choosen_part $mount_point 2>/dev/null
+
+            if [[ $? -eq 0 ]]
+            then
+                notify-send "Mounted $choosen_part at $mount_point"
+            else
+                notify-send "$choosen_part not mounted at $mount_point"
+            fi
+        fi
+    elif [[ $choice = "2) Encrypted" ]]
+    then
+        echo "2)"
+    fi
+elif [[ $choice = "3) Umount partition" ]]
+then
+    umount_prompt="1) Non-encrypted\n2) Encrypted"
+
+    choice=$(echo -e $umount_prompt | $dmenu_command -p "Choose Non-encrypted or Encrypted: ")
+
+    if [[ $choice = "1) Non-encrypted" ]]
+    then
+        choosen_part=$(lsblk -lp | grep "part /" | awk '{print $1, "(" $4 ")"}' | \
+            $dmenu_command -p "Choose partition: ")
+
+        if [[ $choosen_part != "" ]]
+        then
+            choosen_part=$(echo $choosen_part | awk '{print $1}')
+
+            sudo umount $choosen_part 2>/dev/null
+
+            if [[ $? -eq 0 ]]
+            then
+                notify-send "Unmounted $choosen_part"
+            else
+                notify-send "$choosen_part not unmounted"
+            fi
+        fi
+    elif [[ $choice = "2) Encrypted" ]]
+    then
+        echo "2)"
+    fi
+fi
+
