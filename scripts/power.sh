1242341c41cf90f04a3ad9204516b17b2628b80f
aravinthyan <aravinthyan@outlook.com>
2021-05-11 08:56:07 +0100

    Initial power script for dmenu
    - Should be able to reboot and shutdown using this script
    
    - Keybinding not assigned to script

---
+++ scripts/power.sh
@@ -0,0 +1,13 @@
+export dmenu_command='dmenu -i -l 10 -nb '#000000' -nf '#ffffff' -sb '#ff8c00' -sf '#ffffff' -bw 3'
+
+power_prompt="1) Reboot\n2) Shutdown"
+
+choice=$(echo -e $power_prompt | $dmenu_command -p "Choose option: ")
+
+if [[ $choice = "1) Reboot" ]]
+then
+    sudo reboot now
+elif [[ $choice = "2) Shutdown" ]]
+then
+    sudo shutdown now
+fi
