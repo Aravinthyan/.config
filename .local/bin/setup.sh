eccc4f41796ef1679a7ebc191e47e58d52ed08c8
aravinthyan <aravinthyan@outlook.com>
2021-06-04 22:51:39 +0100

    Script executes as root instead of USER

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -1,30 +1,22 @@
-read -sp "Enter Password: " password
-
-echo $password | sudo -kS true
-
-# if password is incorrect exit
-if [[ $? != 0 ]]
-then
-	echo "Incorrect password"
-	exit 1
-fi
+# $1 - $USER
+# $2 - $HOME
 
 ####################################
 # APT
 ####################################
 
 # install packages from apt
-echo $password | sudo -kS apt update
-echo $password | sudo -kS apt install -y build-essential xorg-dev feh maim xclip x11-apps gdm3 \
-    zathura ranger firefox-esr texlive rubber automake gettext libtool libtool-bin autoconf cmake \
-    pkg-config unzip htop dunst fonts-hack sxhkd blueman ntfs-3g libnotify-bin ufw vlc
+apt update
+apt install -y build-essential xorg-dev feh maim xclip x11-apps gdm3 zathura ranger firefox-esr \
+    texlive rubber automake gettext libtool libtool-bin autoconf cmake pkg-config unzip htop dunst \
+    fonts-hack sxhkd blueman ntfs-3g libnotify-bin ufw vlc
 
 # install element
-echo $password | sudo -kS apt install -y wget apt-transport-https
-echo $password | sudo -kS wget -O /usr/share/keyrings/riot-im-archive-keyring.gpg https://packages.riot.im/debian/riot-im-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/riot-im-archive-keyring.gpg] https://packages.riot.im/debian/ default main" | sudo tee /etc/apt/sources.list.d/riot-im.list
-echo $password | sudo -kS apt update
-echo $password | sudo -kS apt install element-desktop -y
+apt install -y wget apt-transport-https
+wget -O /usr/share/keyrings/riot-im-archive-keyring.gpg https://packages.riot.im/debian/riot-im-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/riot-im-archive-keyring.gpg] https://packages.riot.im/debian/ default main" | tee /etc/apt/sources.list.d/riot-im.list
+apt update
+apt install element-desktop -y
 
 ####################################
 # COMPILE AND INSTALL FROM SOURCE
@@ -37,10 +29,12 @@ mkdir $2/tmp && cd $2/tmp
 git clone https://github.com/neovim/neovim.git
 cd neovim
 make CMAKE_BUILD_TYPE=Release
-echo $password | sudo -kS make install
+make install
+
+cd $2/
+rm -rf tmp
 
 cd $2/.config
-rm -rf tmp
 
 ####################################
 # COMPILE AND INSTALL SUBMODULES
@@ -51,18 +45,18 @@ git submodule init
 git submodule update --recursive
 # install dwm
 cd dwm
-echo $password | sudo -kS make clean install
-echo $password | sudo -kS ln -s $2/.config/dwm/dwm.desktop /usr/share/xsessions/dwm.desktop
-echo $password | sudo -kS ln -s $2/.config/dwm/dwm.sh /usr/local/bin/dwm.sh
+make clean install
+ln -s $2/.config/dwm/dwm.desktop /usr/share/xsessions/dwm.desktop
+ln -s $2/.config/dwm/dwm.sh /usr/local/bin/dwm.sh
 # install dmenu
 cd ../dmenu
-echo $password | sudo -kS make clean install
+make clean install
 # install st
 cd ../st
-echo $password | sudo -kS make clean install
+make clean install
 # install slock
 cd ../slock
-echo $password | sudo -kS make clean install
+make clean install
 cd $2
 
 ####################################
@@ -82,12 +76,14 @@ wget -P $2/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master
 # update sudoers list so that USER does not need to type password for shutdown and reboot
 shutdown_cmd=$(whereis shutdown | awk '{print $2}')
 reboot_cmd=$(whereis reboot | awk '{print $2}')
-echo $1" ALL=NOPASSWD:"$shutdown_cmd","$reboot_cmd | sudo EDITOR='tee -a' visudo
+echo $1" ALL=NOPASSWD:"$shutdown_cmd","$reboot_cmd | EDITOR='tee -a' visudo
 
 # create default directories
 mkdir Desktop Documents Downloads Pictures
 
 # configure firewall
-echo $password | sudo -kS ufw default deny incoming
-echo $password | sudo -kS ufw default allow outgoing
+ufw default deny incoming
+ufw default allow outgoing
 
+# ensure USER home directory is owned by USER
+chown -R $USER:$USER $2
