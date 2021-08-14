ab7e914fea96636d127f0c45fefb2829f53b9a9c
Aravinthyan <aravinthyan@outlook.com>
2021-08-14 21:08:48 +0100

    Updated get and set volume to support multiple sinks

--- .local/bin/get_volume.sh
+++ .local/bin/get_volume.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-# get active port
-active_port=$(pactl list sinks | grep "Active Port:" | awk '{print $3}')
+get_line_from_multiline_string() {
+	local line=$(sed -n ${active_sink}p <<< $1)
+	echo $line
+}
 
-# set the appropriate name for active_port
-[[ $active_port = "analog-output-speaker" ]] && active_port="Speaker"
-[[ $active_port = "analog-output-headphones" ]] && active_port="Headphones"
+# get sink list
+sink_info=$(pactl list sinks)
 
-# check if audio is mute or not
-mute=$(pactl list sinks | grep "Mute:" | awk '{print $2}')
+# get active sink
+active_sink=$(echo "$sink_info" | grep -P "^\tState:" | grep -n RUNNING | cut -d: -f1)
+
+# get device description
+device_description=$(get_line_from_multiline_string "`echo "$sink_info" | grep -P "^\t\tdevice.description"`")
+device_description=$(grep -oP '"\K.*?(?=")' <<< "$device_description") # strip to get the device only without the field
+
+# get mute info from all the sinks
+mute_info=$(echo "$sink_info" | grep -P "^\tMute:")
+
+# get mute for specific sink
+mute=$(get_line_from_multiline_string "$mute_info")
+mute=$(echo "$mute" | awk '{print $2}')
 
 # if mute, echo and exit
-[[ $mute = "yes" ]] && echo "Mute - $active_port" && exit 0
+[[ $mute = "yes" ]] && echo "Mute - $device_description" && exit 0
 
-# get volume
-volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | \
-	tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+# get volume of active sink
+volume=$(get_line_from_multiline_string "`echo "$sink_info" | grep -P "^\tVolume:" | awk '{print $5}'`")
 
-echo "Volume: $volume% - $active_port"
+echo "Volume: $volume - $device_description"
--- .local/bin/volume.sh
+++ .local/bin/volume.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
+get_line_from_multiline_string() {
+	local line=$(sed -n ${active_sink}p <<< $1)
+	echo $line
+}
+
+# get sink list
+sink_info=$(pactl list sinks)
+
+# get active sink
+active_sink=$(echo "$sink_info" | grep -P "^\tState:" | grep -n RUNNING | cut -d: -f1)
+
+# get all the sink numbers
+sinks=$(echo "$sink_info" | grep -P "^Sink #")
+
+# get sink
+sink=$(get_line_from_multiline_string "$sinks")
+sink=$(echo "$sink" | cut -d# -f2)
+
 # if passed value is 2 then toggle mute
-[[ $1 = "2" ]] && pactl set-sink-mute 0 toggle && pkill -RTMIN+9 dwmblocks && exit 0
+[[ $1 = "2" ]] && pactl set-sink-mute ${sink} toggle && pkill -RTMIN+9 dwmblocks && exit 0
 
 # if passed value is 0 then decrease volume by 5%
-[[ $1 = "0" ]] && pactl set-sink-volume 0 -5% && pkill -RTMIN+9 dwmblocks && exit 0
+[[ $1 = "0" ]] && pactl set-sink-volume ${sink} -5% && pkill -RTMIN+9 dwmblocks && exit 0
 
 # if passed value is not 0, 1 or 2, exit
 [[ $1 != "1" ]] && exit 0
 
+# get volume of active sink
+volume=$(get_line_from_multiline_string "`echo "$sink_info" | grep -P "^\tVolume:" | awk '{print $5}' | cut -d% -f1`")
+
 # if current volume is 100, exit
-[[ `pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'` = "100" ]] && exit 0
+[[ "$volume" == "100" ]] && exit 0
 
 # this means that the passed value is 1 and the current volume is less than 100
-pactl set-sink-volume 0 +5%
+pactl set-sink-volume ${sink} +5%
 pkill -RTMIN+9 dwmblocks
